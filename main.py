fb510af9ac74590d9e0ca4371b2a6e3ce0c6b37e
andreszp-exertus <84107468+andreszp-exertus@users.noreply.github.com>
2021-07-11 19:46:37 -0500

    Init

---
+++ main.py
@@ -0,0 +1,51 @@
+import openpyxl
+from pathlib import Path
+from collections import defaultdict
+
+informacion = {}
+
+def leerExcel():
+    pass
+
+def iniciarNodos():
+    pass
+
+def printNodeInfo():
+    pass
+
+def printTreeInfo():
+    pass
+
+def crearArbol():
+    pass
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    leerExcel()
+    iniciarNodos()
+    crearArbol()
+
+
+class Node:
+    ciudadOrigen = ""
+    ciudadDestino = ""
+    toneladas = 0
+    viajes = 0
+
+    toneladasEfectivas = 0
+    viajesEfectivos = 0
+    ciudadFinal = ""
+    ciudadPapa = None
+    hijos = []
+
+    def backPropogate(self):
+        pass
+
+    def crearHijos(self):
+        pass
+
+    def esFinal(self):
+        pass
+
+
+
